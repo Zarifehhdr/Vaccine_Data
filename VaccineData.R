7ebd48297d9847c1b159780e888071443333b578
Zarifeh Heidari Rarani <zah47@pitt.edu>
2023-05-14 22:11:17 -0400

    Update VaccineData.R

--- VaccineData.R
+++ VaccineData.R
@@ -1,3 +1,10 @@
-# Start to work on vaccine Data
-G = 2+3
-print("G: ", G)
+# Load the data from the RDS file
+install.packages("readRDS")
+library(readRDS)
+data_extended_old <- readRDS("~/Downloads/VaccineData/extendedOld_norm_batchCorrectedFromYoung_withResponse_eset.rds")
+data_young <- readRDS("~/Downloads/VaccineData/young_norm_withResponse_eset.rds")
+data_old <- readRDS("~/Downloads/VaccineData/old_norm_batchCorrectedFromYoung_withResponse_eset.rds")
+#data_all < -readRDS("~/Downloads/VaccineData/all_norm_withResponse_eset.rds")
+
+# View the data
+head(my_data)
